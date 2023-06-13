d3c5a42d3eddd44f6a4bc12dc9edb4c939e7b532
BRiveau <briveau@uri.edu>
2023-06-13 14:17:12 -0400

    Add project files.

---
+++ Controllers/RssController.cs
@@ -0,0 +1,27 @@
+﻿// Ignore Spelling: Rss API
+
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace RSS_Feed_API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class RssController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult> Get(string url)
+        {
+            var client = new HttpClient();
+            var response = await client.GetAsync(url);
+            var content = await response.Content.ReadAsStringAsync();
+
+            return Content(content, "application/rss+xml");
+        }
+
+    }
+}
